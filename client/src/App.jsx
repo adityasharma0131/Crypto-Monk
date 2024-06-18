ee2d1aef48b507de0c838df8ee878707d3483f25
adityasharma0131 <adityasharma0431@gmail.com>
2024-06-18 13:13:41 +0530

    pages components css and navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,12 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import Homepage from "./pages/Homepage";
-import Coinpage from "./components/Coinpage";
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/coins/:id" element={<Coinpage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
-};
+    <div className='app'>
+      <Navbar />
+    </div>
+  )
+}
 
-export default App;
+export default App
